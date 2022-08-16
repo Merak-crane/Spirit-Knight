3d5bdf5e8e6469f7e79cd1e139df30fd1e5a1493
Lacia <Lacia7366293806@163.com>
2022-08-16 14:58:50 +0800

    窗口基本设置01--from Lacia

--- SpiritKnight02/MyMainWindow.cpp
+++ SpiritKnight02/MyMainWindow.cpp
@@ -1,9 +1,16 @@
-#include "MyMainWindow.h"
-
+﻿#include "MyMainWindow.h"
+#include <QDesktopWidget>
+#include <QDebug>
 MyMainWindow::MyMainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    QDesktopWidget w;
+    int DeskWidth = w.width()/2;
+    int DeskHeight = w.height()/2;
+    this->setFixedSize(DeskWidth, DeskHeight);//设置窗口大小
+    this->setWindowTitle("英灵骑士");//设置窗口标题
+    this->setWindowIcon(QIcon(":/icon/Resource/icon/htmlogo.png"));//
 }
 
 MyMainWindow::~MyMainWindow()
--- SpiritKnight02/MyMainWindow.h
+++ SpiritKnight02/MyMainWindow.h
@@ -10,6 +10,7 @@ class MyMainWindow : public QMainWindow
 public:
     MyMainWindow(QWidget *parent = nullptr);
     ~MyMainWindow();
+    void InitScene();
 
 private:
     Ui::MyMainWindowClass ui;
