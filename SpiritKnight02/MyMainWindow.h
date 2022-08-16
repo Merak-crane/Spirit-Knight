ef511d9b084223b06a4c70ed2edbdb75609a5bba
Lacia <Lacia7366293806@163.com>
2022-08-16 14:09:03 +0800

    添加项目文件。

---
+++ SpiritKnight02/MyMainWindow.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_MyMainWindow.h"
+
+class MyMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MyMainWindow(QWidget *parent = nullptr);
+    ~MyMainWindow();
+
+private:
+    Ui::MyMainWindowClass ui;
+};
