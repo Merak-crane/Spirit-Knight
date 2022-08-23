2dc9b22654ac668184f396f2fb29a564ff7b6546
Lacia <Lacia7366293806@163.com>
2022-08-24 00:08:11 +0800

    界面还可以

--- SpiritKnight02/Start.cpp
+++ SpiritKnight02/Start.cpp
@@ -12,6 +12,7 @@
 Start::Start(QWidget *parent)
 	: QWidget(parent)
 {
+	kind = 0;
 	QDesktopWidget w;
 	int DeskWidth = w.width() / 2;
 	int DeskHeight = w.height() / 2;//获取设备的分辨率
@@ -19,16 +20,23 @@ Start::Start(QWidget *parent)
 	this->setWindowTitle(QString::fromUtf16(u"英灵骑士"));//设置窗口标题
 	this->setWindowIcon(QIcon(":/icon/Resource/icon/htmlogo.png"));//设置窗口logo
 		// 布局界面
-	QMediaPlayer qtaudioPlayer;
-	QVideoWidget* m_videoW = new QVideoWidget(this);
-	m_videoW = new QVideoWidget();
+	qtaudioPlayer = new QMediaPlayer;
+	m_videoW = new QVideoWidget(this);
+	m_videoW->resize(1280, 800);
 	QString path = QDir::currentPath();
 	// 设置视频文件，并播放程序所在目录下的video.mp4
-	qtaudioPlayer.setVideoOutput(m_videoW);
+	qtaudioPlayer->setVideoOutput(m_videoW);
+	m_videoW->setAspectRatioMode(Qt::IgnoreAspectRatio);
+	m_videoW->show();
 	qDebug() << path;
-	qtaudioPlayer.setMedia(QUrl::fromLocalFile("./Resource/image/start_animation.mp4"));
+	qtaudioPlayer->setMedia(QUrl::fromLocalFile("./Resource/image/start_animation.mp4"));
 	qDebug() << path;
-	qtaudioPlayer.play();
+	qtaudioPlayer->play();
+	connect(qtaudioPlayer, &QMediaPlayer::mediaStatusChanged, [=]() {
+		if (qtaudioPlayer->mediaStatus() == 7 ) {
+			m_videoW->close();
+		}
+		});
 	////将QVideoWidget 对象添加到布局
 	//m_layout->addWidget(m_videoW);
 	////设置页面布局
@@ -51,27 +59,38 @@ Start::Start(QWidget *parent)
 	continued->setIcon(p2);
 	continued->setIconSize(QSize(240, 80));
 	continued->setFlat(true);
-	help->resize(240, 80);
-	help->move(50, 650);
+	introduction->resize(240, 80);
+	introduction->move(50, 650);
 	QPixmap p3 = QPixmap(":/icon/Resource/icon/htmlogo.png");
-	help->setIcon(p3);
-	help->setIconSize(QSize(240, 80));
-	help->setFlat(true);
+	introduction->setIcon(p3);
+	introduction->setIconSize(QSize(240, 80));
+	introduction->setFlat(true);
 	exit->resize(240, 80);
 	exit->move(300, 650);
 	QPixmap p4 = QPixmap(":/icon/Resource/icon/htmlogo.png");
 	exit->setIcon(p4);
 	exit->setIconSize(QSize(240, 80));
 	exit->setFlat(true);
-	//connect(exit, &QPushButton::clicked, this, &QWidget::Close);
-	//connect(start, &QPushButton::clicked, this, &Start::Modechoose);
-	//connect(continued, &QPushButton::clicked, this, &Start::openload);
+	mode_one->resize(240, 80);
+	mode_one->move(300, 650);
+	QPixmap p5 = QPixmap(":/icon/Resource/icon/htmlogo.png");
+	mode_one->setIcon(p4);
+	mode_one->setIconSize(QSize(240, 80));
+	mode_one->setFlat(true);
+	connect(exit, &QPushButton::clicked, this, &QWidget::close);
+	connect(start, &QPushButton::clicked, this, &Start::ModeChoose);
+	connect(continued, &QPushButton::clicked, this, &Start::OpenLoad);
 	//connect(returnbtn, &QPushButton::clicked, this, &Start::returnback);
 	//connect(start, &QPushButton::clicked, this, &QWidget::close);
 	//connect(load1btn, &QPushButton::clicked, this, &Start::load_1);
 	//connect(load2btn, &QPushButton::clicked, this, &Start::load_2);
 	//connect(load3btn, &QPushButton::clicked, this, &Start::load_3);
-	//connect(help, &QPushButton::clicked, this, &Start::openhelp);
+	connect(introduction, &QPushButton::clicked, this, &Start::OpenIntroduction);
+	start_time.setInterval(1);
+	start_time.start();
+	connect(&start_time, &QTimer::timeout, [=]() {
+		update();
+	});
 }
 
 Start::~Start()
@@ -79,13 +98,31 @@ Start::~Start()
 
 void Start::paintEvent(QPaintEvent* Event) {
 	QPainter painter1(this);
-	painter1.drawPixmap(0,0, this->width(), this->height(), QPixmap(":/image/Resource/image/background/background.png"));
+	if (kind == 0) {
+		painter1.drawPixmap(0, 0, this->width(), this->height(), QPixmap(":/image/Resource/image/background/background.png"));
+	}
+	if (kind == 1) {
+		painter1.drawPixmap(0, 0, this->width(), this->height(), QPixmap(":/image/Resource/image/background/background2.jpg"));
+	}
+	if (kind == 2) {
+		painter1.drawPixmap(0, 0, this->width(), this->height(), QPixmap(":/image/Resource/image/background/modechoose.jpg"));
+	}
+	if (kind == 4) {
+		painter1.drawPixmap(0, 0, this->width(), this->height(), QPixmap(":/image/Resource/image/background/background.jpeg"));
+	}
 }
 
-//void Start::modechoose(){
-//
-//}
-//
+void Start::ModeChoose(){
+	kind = 1;
+}
+
+void Start::OpenLoad() {
+	kind = 2;
+}
+
+void Start::OpenIntroduction() {
+	kind = 4;
+}
 //void Start::close() {
 //
 //}
--- SpiritKnight02/Start.h
+++ SpiritKnight02/Start.h
@@ -2,6 +2,12 @@
 
 #include <QWidget>
 #include <QPushButton>
+#include <QMediaPlaylist>
+#include <QMediaPlayer>
+#include <QDir>
+#include <QVideoWidget>
+#include <QTimer>
+
 
 
 class Start : public QWidget
@@ -13,14 +19,21 @@ public:
 	~Start();
 	void paintEvent(QPaintEvent* Event);
     void ModeChoose();
+    void OpenLoad();
+    void OpenIntroduction();
     void Close();
+    int kind;
     QPushButton* start = new QPushButton(this);
+    QPushButton* mode_one = new QPushButton(this);
     QPushButton* continued = new QPushButton(this);
-    QPushButton* help = new QPushButton(this);
+    QPushButton* introduction = new QPushButton(this);
     QPushButton* exit = new QPushButton(this);
     QPushButton* returnbtn = new QPushButton(this);
     QPushButton* load1btn = new QPushButton(this);
     QPushButton* load2btn = new QPushButton(this);
     QPushButton* load3btn = new QPushButton(this);
 private:
+    QMediaPlayer* qtaudioPlayer;
+    QVideoWidget* m_videoW;
+    QTimer start_time;
 };
